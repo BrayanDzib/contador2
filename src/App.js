280ecfbf3f0cd3f726912e4fa96b955296ca4ce7
BrayanDzib <20470109@campeche.tecnm.mx>
2023-06-01 12:14:08 -0600

    contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incrementar = () => {
+    setCount(count + 1);
+  };
+
+  const decrementar = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div >
+      <h2>Contador </h2>
+      <h1> {count}</h1>
+      <button onClick={decrementar}>-</button>
+      <button onClick={incrementar}>+</button>
+      <button onClick={reset}>Reiniciar</button>
     </div>
   );
 }
 
-export default App;
+export default Counter
